1fbf9b9b4b08f6e1d6c06dbd5ca9fc53c48cff92
kelvinnpc <kelvinnpc@gmail.com>
2016-12-03 00:29:00 +0800

    door and keys

--- Assets/Scripts/Soul.cs
+++ Assets/Scripts/Soul.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Soul : MonoBehaviour {
 
 	public float speed;
+	public int key;
 	Rigidbody2D rgbody;
 	[SerializeField]
 	private Player playerScript;
@@ -12,6 +13,7 @@ public class Soul : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rgbody = GetComponent<Rigidbody2D> ();
+		key = 0;
 	}
 
 	// Update is called once per frame
